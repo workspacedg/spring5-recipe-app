cd220d468392c4bce23958d0b4e635249938dd93
lexx <jrlexx@hotmail.com>
2020-01-06 18:25:56 -0500

    Services

--- src/main/java/guru/springframework/domain/Notes.java
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -12,5 +12,33 @@ public class Notes {
 
     @OneToOne
     private Recipe recipe;
+    @Lob
     private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Notes setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public Notes setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+        return this;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public Notes setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+        return this;
+    }
 }
